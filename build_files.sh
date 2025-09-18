8a82862e676f0cfdbb6a8b6f88bfeed507358782
Loric Audin <loricaudin@yahoo.fr>
2025-09-18 20:33:19 +0200

    Test 26 Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # Pour Vercel uniquement
-mkdir -p staticfiles
-pip3 install -r requirements.txt
-python3 manage.py collectstatic --noinput
-
-mkdir -p /vercel/output/static
-cp -r staticfiles/. /vercel/output/static/
+pip install -r requirements.txt
+mkdir -p public/static
+python manage.py collectstatic --noinput
