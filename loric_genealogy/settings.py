2f72ddd4038d6ac03c5d47f3f36ebd1cc5643a6f
Loric Audin <loricaudin@yahoo.fr>
2025-09-18 17:49:57 +0200

    Correct erreur settings

--- loric_genealogy/settings.py
+++ loric_genealogy/settings.py
@@ -36,7 +36,7 @@ if env('ENV') == 'dev':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
@@ -143,9 +143,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
+STATICFILES_DIRS = [BASE_DIR/'static']
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
 MEDIA_URL = '/media/'
