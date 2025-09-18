874956cdbec6b7e9cf7a6538312be7a32f66899c
Loric Audin <loricaudin@yahoo.fr>
2025-09-18 19:49:45 +0200

    Déplacement static vers dossier public (test)

--- loric_genealogy/settings.py
+++ loric_genealogy/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR "public" / "static"
 
 
 MEDIA_URL = '/media/'
