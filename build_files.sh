9006c0972bd94d8c250e241cac1cafc42f41a499
Loric Audin <loricaudin@yahoo.fr>
2025-09-18 19:39:52 +0200

    test affichage Dossiers Vercel

--- build_files.sh
+++ build_files.sh
@@ -5,9 +5,7 @@ mkdir -p staticfiles
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
 
-echo Contenu path0 :
-ls /vercel/path0
-echo Contenu output :
-ls /vercel/output
-echo Conenu builds.json :
-cat /vercel/output/builds.json
+echo Contenu vercel :
+ls /vercel/
+echo Contenu / :
+ls /
