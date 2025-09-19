c15a12aa000d4d7324c6b7eb2109ac3224d4aaa9
Loric Audin <loricaudin@yahoo.fr>
2025-09-19 15:40:11 +0200

    Correction taille titre petits écrans

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Pour Vercel uniquement
+echo DEBUT BUILD VERCEL
+
 pip3 cache purge
 pip3 install setuptools
 pip3 install -r requirements.txt
@@ -8,3 +10,5 @@ pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
+
+echo FIN BUILD VERCEL
